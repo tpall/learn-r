bed251f1cdbb1c35d32efbe99f75ee9598fb5207
Taavi Päll <tapa741@gmail.com>
2016-09-01 13:15:33 +0300

    save without date

--- src/nr_of_cran_packages.R
+++ src/nr_of_cran_packages.R
@@ -5,4 +5,4 @@ library(rvest) # if missing run install.packages(c("xml2", "rvest"))
 url <- "https://cran.r-project.org/web/packages/available_packages_by_date.html"
 h <- read_html(url)
 t <- html_table(h)
-save(t, file = paste0("data/Available_CRAN_Packages_By_Date_of_Publication_",Sys.Date(),".RData"))
+save(t, file = paste0("data/Available_CRAN_Packages.RData"))
